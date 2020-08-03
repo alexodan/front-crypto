b5375bc7be22df0a0c8090ad89ae467e1c195b51
alexodan <alexdan1993@gmail.com>
2020-08-03 10:49:33 -0300

    Fixing import

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // https://be.buenbit.com/api/market/tickers/
 // https://api.satoshitango.com/v3/ticker/ALL
 // https://api.exchange.ripio.com/api/v1/rate/BTC_ARS/
-import { getBuenbitDaiData, getBuenbitBtcData } from './utils.js';
+import { getBuenbitDaiData, getBuenbitBtcData } from './api.js';
 import { drawChart } from './chart.js';
 
 // Either Null / AggregatedInfo
